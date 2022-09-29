a7b3d1a670d6ed65e5656444100b344ab245563b
Abhilashax <abhinanda361@gmail.com>
2022-09-29 14:03:18 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Faq from "./components/Faq";
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Navbar />
       <Faq />
     </div>
   );
