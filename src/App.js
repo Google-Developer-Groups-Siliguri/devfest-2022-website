6c27c9196312e403fcbdf6d4b33912b82acbae49
Abhilashax <abhinanda361@gmail.com>
2022-09-29 16:28:25 +0530

    Added Hero->Hero Text+Timer+Bottom Monuments Image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Faq from "./components/Faq";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero/>
       <Faq />
     </div>
   );
