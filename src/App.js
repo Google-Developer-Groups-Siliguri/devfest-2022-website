7b95a12e89712e003de9d745907583999f91497f
Abhilashax <abhinanda361@gmail.com>
2022-09-30 14:14:46 +0530

    Fixed a small bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Faq from "./components/Faq";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/NavBar"
 
 function App() {
   return (
