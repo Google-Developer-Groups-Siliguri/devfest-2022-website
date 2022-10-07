73db458b994ddb9601bc239bb213d722efd6295a
Soumwadeep Guha <soumwadeep@gmail.com>
2022-10-07 17:07:46 +0530

    Added Sponsors And Partners

---
+++ src/components/Sponsors.js
@@ -0,0 +1,67 @@
+import React from "react";
+import googledevelopers from "../assets/images/googledevelopers.png";
+import "../stylesheet/Sponsor.css";
+const Sponsors = () => {
+  return (
+    <section id="sponsors">
+      <div className="container">
+        <br />
+        <center>
+          <h1 style={{ fontSize: "4rem" }}>Sponsors</h1>
+        </center>
+        <div className="row">
+          <center>
+            <div className="cardbox1">
+              <img src={googledevelopers} className="cardimgfix" alt="" />
+              <div className="bottombox1">
+                <p>Title Sponsor</p>
+              </div>
+            </div>
+          </center>
+        </div>
+        <br />
+        <div className="row">
+          <center>
+            <div className="cardbox2">
+              <img src={googledevelopers} className="cardimgfix" alt="" />
+              <div className="bottombox2">
+                <p>Gold Sponsor</p>
+              </div>
+            </div>
+          </center>
+        </div>
+        <br />
+        <center>
+          <div className="row">
+            <div className="col-sm">
+              <div className="cardbox3">
+                <img src={googledevelopers} className="cardimgfix" alt="" />
+                <div className="bottombox3">
+                  <p>Silver Sponsor</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="cardbox3">
+                <img src={googledevelopers} className="cardimgfix" alt="" />
+                <div className="bottombox3">
+                  <p>Silver Sponsor</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="cardbox3">
+                <img src={googledevelopers} className="cardimgfix" alt="" />
+                <div className="bottombox3">
+                  <p>Silver Sponsor</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </center>
+      </div>
+    </section>
+  );
+};
+
+export default Sponsors;
