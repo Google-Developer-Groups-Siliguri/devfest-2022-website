e14571cdf6459e27e6e14c1958dc357b1bbc588b
alokkumax <alokkumax1@gmail.com>
2022-10-09 19:43:04 +0530

    fixed some css of HeroSection

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,28 +8,27 @@ import { BsCalendarCheck } from "react-icons/bs";
 import { MdLocationPin } from "react-icons/md";
 
 export default function Hero() {
-  return (
-    <div>
-        <div className="hero">
-            <div className="container">
-                <div className="row align-items-center pt-1">
-                <div className="col-lg-7 col-12">
-                    <img src={logo} className="logo" alt="" />
-                    <p className="lead">Google Developer Groups is back with DevFest in your own town Siliguri, GDG Siiguri is organising this event in Siliguri, Do register and lets meet at the Devfest!</p>
-                    <p className="lead-2"><BsCalendarCheck/> 5th &amp; 6th November</p>
-                    <p className="lead-2"><MdLocationPin/>  Siliguri Institute of Technology</p>
-                    
-                    <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" className="btn btn-df btn-lg px-4 me-md-2">Register</button>
+    return (
+        <div>
+            <div className="hero">
+                <div className="heroBox">
+                    <div className="row justify-content-between">
+                        <div className="col-lg-6 col-12">
+                            <img src={logo} className="logo" alt="" />
+                            <p className="lead">Google Developer Groups is back with DevFest in your own town Siliguri, GDG Siiguri is organising this event in Siliguri, Do register and lets meet at the Devfest!</p>
+                            <p className="lead-2"><BsCalendarCheck /> 5th &amp; 6th November</p>
+                            <p className="lead-3"><MdLocationPin />  Siliguri Institute of Technology</p>
+                            <div className="d-grid gap-2 d-md-flex justify-content-md-start my-4">
+                                <button type="button" className="btns">Register</button>
+                            </div>
+                        </div>
+                        <div className="col-lg-5 col-12">
+                            <Countdown />
+                        </div>
                     </div>
                 </div>
-                <div className="col-lg-5 col-12">
-                    <Countdown/>
-                </div>
-                </div>
             </div>
+            <img src={monuments} className="monuments-img"></img>
         </div>
-         <img src={monuments} className="monuments-img"></img>
-    </div>
-  )
+    )
 }
