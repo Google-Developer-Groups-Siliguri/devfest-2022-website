081ff3a4478de70c0b590ab120fc114b72e985a0
Abhilashax <abhinanda361@gmail.com>
2022-10-19 13:40:42 +0530

    Fixed contact page spelling mistakes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,14 +11,14 @@ export default function Contact() {
       <div className='page'>
         <div className='content'>
           {/* <p className='container text-center mt-5 dev-heading'>Reach out to us for any enquiry</p> */}
-          <p className='container text-center mt-5 head'>Google Developers Groups Siliguri</p>
-          <p className='container text-center head-1'>The Community of Developers, For Developers, By Developers</p>
+          <p className='container text-center mt-5 head'>Google Developers Group, Siliguri</p>
+          <p className='container text-center head-1'> Of the Developers, By the Developers, For the Developers</p>
           <br />
           <p className='container text-center head-1'>Google Developer Group Siliguri is a Siliguri community-run meetup for developers interested in resources and technology from Google Developers. We try to engage student developers, fresh graduates and professionals through our hack events & meetups.
           </p>
           <br />
           <p className='container text-center head-1'>
-            The motive is to create a local ecosystem of programmers & hackers in and around Bhopal. The technology awareness is main goal for first few years of the group. To put it in a nutshell, we are a community that organizes tech talks, workshops, bootcamps, Google flagship events where programmers meet to discuss and work with several Google developer resources and products.
+            The motive is to create a local ecosystem of programmers & hackers in and around Siliguri. The technology awareness is main goal for first few years of the group. To put it in a nutshell, we are a community that organises tech talks, workshops, bootcamps, Google flagship events where programmers meet to discuss and work with several Google developer resources and products.
           </p>
           <br />
           <div className='container text-center'>
