5bb4605420d1589e5d4c0ea3e9cac9159a65325a
alokkumax <alokkumax1@gmail.com>
2022-10-28 15:11:44 +0530

    added sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -4,7 +4,7 @@ import sitlogo from "../assets/images/sitlogo.png";
 import taskade from "../assets/images/SponsorsLogo/taskade.png";
 import assetmantle from "../assets/images/SponsorsLogo/assetmantle.jpeg";
 import kalimpongstore from "../assets/images/SponsorsLogo/kalimpongstore.png";
-import cyber from  "../assets/images/SponsorsLogo/cyber.png";
+import cyber from "../assets/images/SponsorsLogo/cyber.png";
 import edugree from "../assets/images/SponsorsLogo/edugree.png";
 import ted from "../assets/images/SponsorsLogo/ted.png";
 import dlabs from "../assets/images/SponsorsLogo/dlabs.jpeg";
@@ -124,6 +124,39 @@ const Sponsors = () => {
             </div>
           </div>
         </center>
+      </div>
+
+      <p className="text-center dev-heading mt-5 mb-5">Our Partners</p>
+      <div className="container">
+        <div className="row">
+          <center className="row space-between">
+            <div className="col">
+              <div className="card sponsorcards card1">
+                <img
+                  src={googledevelopers}
+                  className="card-img-top img1"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title title1">Digital Partner</h5>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card sponsorcards card1">
+                <img
+                  src={googledevelopers}
+                  className="card-img-top img1"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title title1">Media Partner</h5>
+                </div>
+              </div>
+            </div>
+          </center>
+        </div>
       </div>
     </section>
   );
